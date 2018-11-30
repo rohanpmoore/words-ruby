00594936b4855ea767a36f663265fc3113830c7e
Rohan Moore <rohanpmoore@gmail.com>
2018-11-30 09:07:05 -0800

    add clear function and database saving

--- lib/word.rb
+++ lib/word.rb
@@ -4,9 +4,13 @@ module Dictionary
     @@dictionary = []
     def initialize(attributes)
       @word = attributes.fetch(:word)
+      @@dictionary.push(self)
     end
     def self.all()
       return @@dictionary
+    end
+    def self.clear()
+      @@dictionary = []
     end
   end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -3,14 +3,27 @@ require('word')
 
 describe('Dictionary') do
   describe('Word') do
+    before() do
+      Dictionary::Word.clear
+    end
     describe('#initialize') do
       it('creates a new word based on user input') do
         word = Dictionary::Word.new({:word=> "airplane"})
         expect(word.word).to(eq('airplane'))
       end
+      it('adds the new word to the database immediately') do
+        word = Dictionary::Word.new({:word=> "telephone"})
+        expect(Dictionary::Word.all).to(eq([word]))
+      end
     end
     describe('.all') do
       it('gets an empty array if no words exist') do
+        expect(Dictionary::Word.all).to(eq([]))
+      end
+    end
+    describe('.clear') do
+      it('clears the database of words') do
+        Dictionary::Word.clear
         expect(Dictionary::Word.all).to(eq([]))
       end
     end
